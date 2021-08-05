36794947cea5941d549f82a7731aefc569408394
slow__rider <akashcsemu@gmail.com>
2021-08-06 00:35:45 +0600

    add media query for small size display and javascript dom functionality to add book information at all books list

---
+++ script.js
@@ -0,0 +1,69 @@
+//Select Element
+const inputForm = document.querySelector('.book-input-form');
+const allBooksTable_body = document.querySelector('#all-table-body');
+
+
+
+// Functions
+let validForm = function(formObject){
+    let ok = false;
+    let title = formObject.querySelector('#title');
+    let author = formObject.querySelector('#author');
+    let category = formObject.querySelector('#category');
+
+    // console.log(title.value);
+    // console.log(author.value);
+    // console.log(category.value);
+    if(title.value && author.value && category.value){
+        ok = true;
+    }
+    return ok;
+};
+
+
+let addItem = function(singleRow){
+    let tableBody = document.querySelector('#all-table-body');
+    tableBody.appendChild(singleRow);
+};
+
+let creteTableItem = function(formObject){
+    // let title = formObject.querySelector('#title');
+    // let author = formObject.querySelector('#author');
+    // let category = formObject.querySelector('#category');
+
+    let tr = document.createElement('tr');
+    
+    for(let i=0;i<formObject.children.length-1;i++){
+        let td = document.createElement('td');
+        td.textContent = formObject.children[i].value;
+        tr.appendChild(td);
+    }
+    addItem(tr);
+
+};
+
+let clearAllinputFill = function(formObject){
+    for(let i=0;i<formObject.children.length-1;i++){
+        formObject.children[i].value = "";
+    }
+};
+
+let addBook = function(event){
+    event.preventDefault();
+    let formObject = event.target;
+    if(validForm(event.target)==true){
+        creteTableItem(event.target);
+        window.alert("Ok bahi thik ase. Ami sob add kore dicci");
+        clearAllinputFill(event.target);
+    }else{
+        window.alert("Bhai sob thik thak dewa hoy nai!");
+    }
+
+};
+
+inputForm.addEventListener('submit',addBook);
+
+
+
+
+
