d520d5481f4b2ae222677ccb62f29b64143dc657
slow__rider <akashcsemu@gmail.com>
2021-08-06 21:53:17 +0600

    Added button functionality with all books table and Favourite Books table row

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Select Element
 const inputForm = document.querySelector('.book-input-form');
 const allBooksTable_body = document.querySelector('#all-table-body');
-
+const favBooksTable_body = document.querySelector('#fav-table-body'); 
 
 
 // Functions
@@ -11,9 +11,6 @@ let validForm = function(formObject){
     let author = formObject.querySelector('#author');
     let category = formObject.querySelector('#category');
 
-    // console.log(title.value);
-    // console.log(author.value);
-    // console.log(category.value);
     if(title.value && author.value && category.value){
         ok = true;
     }
@@ -27,7 +24,56 @@ let addItem = function(singleRow){
     setTimeout(() => {
         window.alert("Ok bahi thik ase. Ami sob add kore dicci");
     }, 1);
+    bindDeleteBtn(singleRow,deleteItem);
+    bindFavouriteBtn(singleRow,addInFavourite);
 };
+
+
+let deleteItem = function(){
+    let item = this.parentNode.parentNode;
+    let tableBody = item.parentNode;
+    tableBody.removeChild(item);
+
+};
+
+
+let addInFavourite = function(){
+    let orginalItem = this.parentNode.parentNode;
+    let item = orginalItem.cloneNode(true);
+    let tdWithButtons = item.querySelector('.options');
+    item.removeChild(tdWithButtons); // item without buttons
+
+    // creating item with undo button 
+    let td = document.createElement('td');
+    td.className='options';
+
+    let undoButton = document.createElement('button');
+    undoButton.setAttribute('id','undoBtn');
+    undoButton.textContent = 'Undo';
+    td.appendChild(undoButton);
+
+    item.appendChild(td);
+
+
+    favBooksTable_body.appendChild(item);
+    bindUndoButton(item,deleteItem);  
+};
+
+let bindDeleteBtn = function(singleRow,deleteButtonClick){
+    let button = singleRow.querySelector('#deleteBtn');
+    button.onclick = deleteButtonClick;
+}
+
+let bindFavouriteBtn = function(singleRow,favouriteButtonClick){
+    let button = singleRow.querySelector('#favBtn');
+    button.onclick = favouriteButtonClick;
+}
+
+let bindUndoButton = function(item,undoButtonClick){
+    let button = item.querySelector('#undoBtn');
+    button.onclick = undoButtonClick;
+}
+
 
 let creteTableItem = function(formObject){
     // let title = formObject.querySelector('#title');
@@ -41,6 +87,21 @@ let creteTableItem = function(formObject){
         td.textContent = formObject.children[i].value;
         tr.appendChild(td);
     }
+    let td = document.createElement('td');
+    td.className = 'options';
+
+    let deleteButton = document.createElement('button');
+    deleteButton.setAttribute('id','deleteBtn');
+    deleteButton.textContent = 'Delete';
+    
+    let favButton = document.createElement('button');
+    favButton.setAttribute('id','favBtn');
+    favButton.textContent='Favourite';
+
+    td.appendChild(deleteButton);
+    td.appendChild(favButton);
+
+    tr.appendChild(td);
     addItem(tr);
 
 };
